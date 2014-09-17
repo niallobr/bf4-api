ca353b939ac05ab3aa6c73aa58c9f05a000198d2
Niazll <niall@Nialls-Mac.local>
2014-09-17 17:23:32 +0100

    Added first request

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
 console.log('\'Allo \'Allo!');
+
+$(function () {
+    $.ajax({
+        type: 'GET',
+        url: 'http://api.bf4stats.com/api/onlinePlayers',
+        success: function(data) {
+          console.log("Players currently online:", data); //returns all online players
+       }
+    });
+});
